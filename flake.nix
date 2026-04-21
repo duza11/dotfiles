3f7b2c6b348cda5d6fa053ca11a89eb0ae0fb017
duza11 <318.abcdothe.ds@gmail.com>
2026-04-21 22:14:49 +0900

    Linuxでdarwin用のnixpkgsを参照する不具合を修正

--- flake.nix
+++ flake.nix
@@ -25,16 +25,24 @@
       ...
     }@inputs:
     let
-      system = "aarch64-darwin";
-      username = builtins.getEnv "USER";
-      pkgs = import nixpkgs { inherit system; };
+      linuxSystem = "x86_64-linux";
+      darwinSystem = "aarch64-darwin";
+
+      linux_username = builtins.getEnv "USER";
+
+      mkPkgs = system: import nixpkgs { inherit system; };
+
+      pkgsLinux = mkPkgs linuxSystem;
+      pkgsDarwin = mkPkgs darwinSystem;
     in
     {
-      formatter.${system} = pkgs.nixfmt;
-      homeConfigurations.${username} = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+      formatter.${linuxSystem} = pkgsLinux.nixfmt;
+      formatter.${darwinSystem} = pkgsDarwin.nixfmt;
+      homeConfigurations.${linux_username} = home-manager.lib.homeManagerConfiguration {
+        pkgs = pkgsLinux;
         extraSpecialArgs = {
-          inherit inputs username;
+          inherit inputs;
+          username = linux_username;
         };
         modules = [
           ./nix/home-manager/common.nix
@@ -42,13 +50,11 @@
         ];
       };
       darwinConfigurations.d-darwin = nix-darwin.lib.darwinSystem {
-        inherit system;
-
+        system = "aarch64-darwin";
         specialArgs = {
           inherit self inputs;
           username = "duza";
         };
-
         modules = [
           ./nix/nix-darwin/default.nix
         ];
