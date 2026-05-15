212ca3ee3f01312a565c04bcd92e20b9114459a7
duza11 <318.abcdothe.ds@gmail.com>
2026-05-15 23:22:29 +0900

    feat(nvim): UI改善系のプラグインを追加

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -25,4 +25,65 @@ return {
       end)
     end,
   },
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'rcarriga/nvim-notify',
+    },
+    opts = {
+      lsp = {
+        progress = {
+          enabled = true,
+        },
+        hover = {
+          enabled = true,
+          silent = true,
+        },
+        signature = {
+          enabled = false,
+        },
+        message = {
+          enabled = true,
+        },
+        documentation = {
+          view = 'hover',
+        },
+      },
+      presets = {
+        command_palette = true,
+        long_message_to_split = true,
+        lsp_doc_border = true,
+      },
+    },
+    config = function(_, opts)
+      require('notify').setup({
+        background_colour = '#000000',
+        merge_duplicates = true,
+      })
+      vim.notify = require('notify')
+      require('noice').setup(opts)
+    end,
+  },
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    main = 'ibl',
+    config = function ()
+      require('ibl').setup()
+    end
+  },
+  {
+    'folke/which-key.nvim',
+    event = 'VeryLazy',
+    keys = {
+      {
+        '<leader>?',
+        function()
+          require('which-key').show({ global = false })
+        end,
+        desc = 'Buffer Local Keymaps (which-key)',
+      },
+    },
+  }
 }
