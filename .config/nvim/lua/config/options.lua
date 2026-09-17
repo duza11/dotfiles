86cefc8ca545880ed95d0ff1336fbbfec2f749e5
duza11 <318.abcdothe.ds@gmail.com>
2026-09-17 21:16:46 +0900

    chore(nvim): クリップボード設定を変更

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -133,3 +133,29 @@ vim.filetype.add({
     tag = 'riot_v3',
   },
 })
+
+-- =========================
+-- Clipboard
+-- =========================
+local osc52 = require('vim.ui.clipboard.osc52')
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = osc52.copy('+'),
+    ['*'] = osc52.copy('*'),
+  },
+  paste = {
+    ['+'] = function()
+      return {
+        vim.split(vim.fn.getreg(''), '\n'),
+        vim.fn.getregtype(''),
+      }
+    end,
+    ['*'] = function()
+      return {
+        vim.split(vim.fn.getreg(''), '\n'),
+        vim.fn.getregtype(''),
+      }
+    end,
+  },
+}
