89f4f7b000002521a2af09daeab039dc2a4adcfe
duza11 <318.abcdothe.ds@gmail.com>
2026-09-17 20:36:13 +0900

    chore(nix,nvim): パッケージやプラグインを更新

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
     herdr = {
-      url = "github:herdrdev/herdr/v0.8.2";
+      url = "github:herdrdev/herdr/v0.9.1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     riot-v3-language-server.url = "github:duza11/riot-v3-language-server";
